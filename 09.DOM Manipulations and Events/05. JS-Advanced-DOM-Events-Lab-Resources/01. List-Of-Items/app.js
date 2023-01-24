d7c197907fbb161ea4ec0a7f19cdebd22c30e6ea
Borislav Dukov <borislav.dukov@icloud.com>
2023-01-24 22:30:54 +0200

    DOM Manipulations and Events
    Exercises from DOM manipulations and events lecture

--- 09.DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
+++ 09.DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,7 @@
 function addItem() {
-    console.log('TODO:...');
+    input = document.getElementById('newItemText');
+    const newLi = document.createElement('li');
+    newLi.textContent = input.value;
+    const list = document.getElementById('items');
+    list.appendChild(newLi);
 }
