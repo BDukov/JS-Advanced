7cd60f446c9caea00a2d011a12c6bd76dbaf6905
Borislav Dukov <borislav.dukov@icloud.com>
2023-01-24 19:13:03 +0200

    Exercise: DOM Introduction

--- 07.DOM Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 07.DOM Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,20 @@
 function sumTable() {
+    // let sum = 0;
+    // let digits = document.getElementsByTagName('tr');
+    // for (let i = 1; i < digits.length - 1; i++) {
+    //     const element = digits[i];
+    //     let num = Array.from(element);
+    //     console.log(num);
+    //     sum += Number(element);        
+    // }
+    // console.log(sum);
+    let costElements = document.querySelectorAll('tr td:nth-of-type(2)');
 
+    let sum = Array.from(costElements).reduce((a, x) => {
+        let currentValue = Number(x.textContent) || 0;
+        return a + currentValue
+    }, 0);
+
+    let resultElement = document.getElementById('sum');
+    resultElement.textContent = sum;
 }
