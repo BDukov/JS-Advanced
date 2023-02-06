feb113f1c719fc4465410592902fc1ca55b30458
Borislav Dukov <borislav.dukov@icloud.com>
2023-02-06 22:25:39 +0200

    Classes
    Exercises from Classes

---
+++ 16.Exercise Classes/08. Juice Flavors.js
@@ -0,0 +1,38 @@
+function juiceFlavors(arr) {
+
+    const juices = {};
+    const juiceBottles = {};
+
+    for (let line of arr) {
+        let [juiceName, juiceQuantity] = line.split(" => ");
+        if (!juices.hasOwnProperty(juiceName)) {
+            juices[juiceName] = 0;
+        }
+        juices[juiceName] += Number(juiceQuantity);
+
+        if (juices[juiceName] >= 1000) {
+            let numberOfBottles = Math.floor(juices[juiceName] / 1000);
+            if (!juiceBottles.hasOwnProperty(juiceName)) {
+                juiceBottles[juiceName] = 0;
+            }
+            juiceBottles[juiceName] += numberOfBottles;
+            juices[juiceName] -= numberOfBottles * 1000;
+        }
+    }
+
+    Object.entries(juiceBottles).forEach((juiceBottle) => console.log(`${juiceBottle[0]} => ${juiceBottle[1]}`));
+
+}
+
+juiceFlavor(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549']);
+
+juiceFlavor(['Kiwi => 234',
+    'Pear => 2345',
+    'Watermelon => 3456',
+    'Kiwi => 4567',
+    'Pear => 5678',
+    'Watermelon => 6789']);
